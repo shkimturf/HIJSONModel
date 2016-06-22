9db5fc9fbc0a31e98a93ed1051909ef3f19198d8
Sunhong Kim <shkim@sunhong-ui-imac.local>
2016-06-23 02:07:49 +0900

    Initial Commit

---
+++ HIJSONModel/HIJSONModel.h
@@ -0,0 +1,13 @@
+//
+//  HIJSONModel.h
+//  HIJSONModel
+//
+//  Created by Sunhong Kim on 2016. 6. 23..
+//  Copyright © 2016년 Sunhong Kim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HIJSONModel : NSObject
+
+@end
---
+++ HIJSONModel/HIJSONModel.m
@@ -0,0 +1,13 @@
+//
+//  HIJSONModel.m
+//  HIJSONModel
+//
+//  Created by Sunhong Kim on 2016. 6. 23..
+//  Copyright © 2016년 Sunhong Kim. All rights reserved.
+//
+
+#import "HIJSONModel.h"
+
+@implementation HIJSONModel
+
+@end
